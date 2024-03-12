10cef09a0b72229ab067c24cf4d503c6d803d337
TudosLevente <pop2014mmc@gmail.com>
2024-03-12 10:19:44 +0100

    login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className='container'>
+            <h2>Login to your account</h2>
+        </div>
+    );
+}
+
+export default Login;
